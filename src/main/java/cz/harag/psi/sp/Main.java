9efb2719f395c65871024caa12af741f6b1b6ec6
Patrik Harag <patrikharag@gmail.com>
2020-05-16 14:43:12 +0200

    Add project structure

---
+++ src/main/java/cz/harag/psi/sp/Main.java
@@ -0,0 +1,24 @@
+package cz.harag.psi.sp;
+
+import java.util.Locale;
+
+/**
+ * Vstupní třída.
+ *
+ * @version 2020-05-16
+ * @author Patrik Harag
+ */
+public class Main {
+
+    /**
+     * Vstupní metoda.
+     *
+     * @param args argumenty
+     */
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+
+        System.out.println("Hello");
+    }
+
+}
